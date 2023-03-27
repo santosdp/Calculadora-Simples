bbff5761aad301ac041da7c9e4caeb70c4fa3f16
Daniel Pereira <santosd.pereira@gmail.com>
2023-03-27 14:58:12 -0400

    Adição de comentários

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+/*Função que faz as operações nos valores selecionados na calculadora*/
 function displaynum(n1){
     Calculadora.text1.value = Calculadora.text1.value + n1;
 }
+/*Função que limpa o display da calculadora ao apertar o botão C*/
 function limpardisplay(){
     Calculadora.text1.value = "";
 }
